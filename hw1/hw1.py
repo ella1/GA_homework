f1e95666d21a0a6a1472130084308f3d78c55d80
ella1 <ella.merjanova@gmail.com>
2013-09-27 04:16:05 +0000

    hw1

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,41 +1,34 @@
-#test
 from __future__ import division
 import numpy as np
 from sklearn import datasets
 from sklearn.neighbors import KNeighborsClassifier
-    
+from sklearn import cross_validation
+
 # Import the data
 iris=datasets.load_iris()
 iris_x=iris.data
 iris_y=iris.target
-print np.unique(iris_y)
+#print np.unique(iris_y)
 
-#Divide the data for cross-validation
-def nFoldSplit(n,iris_x,iris_y,iris_x_train,iris_y_train,iris_x_test,iris_y_test):
-        np.random.seed(0)  #randomize the data rows
-        indices=np.random.permutation(len(iris_x)) #len(iris_x)= 150
-        iris_x_train=iris_x[indices[:-math.trunc((len(iris_x)/n))]]
-        iris_y_train=iris_x[indices[:-math.trunc((len(iris_y)/n))]]
-        iris_x_test=iris_x[indices[math.trunc((len(iris_x)/n)):]]
-        iris_y_test=iris_x[indices[math.trunc((len(irix_y)/n)):]]
-        return iris_x_test,iris_y_test,iris_x_train,iris_y_train
+#Divide the data for validation
 
-#call the split function
+k = [5, 15]
+SEED = 42
+n = 10
+def nFoldSplit(k):#n,iris_x,iris_y,iris_x_train,iris_y_train,iris_x_test,iris_y_test):
+	for i in range(n):
+		iris_x_train, iris_x_test, iris_y_train, iris_y_test = cross_validation.train_test_split(iris_x, iris_y, test_size=0.2, random_state=i*SEED)
 
-nFoldSplit(10,iris_x,iris_y,iris_x_train,iris_y_train,iris_x_test,iris_y_test)
+#Create the classifier and fit a model to it
+		knn = KNeighborsClassifier(n_neighbors=k)
+		knn.fit(iris_x_train,iris_y_train)
+		iris_y_prediction=knn.predict(iris_x_test)
+#Calculate the accuracy of the model
+		accuracy = knn.score(iris_x_test, iris_y_test)
+		#accuracy=np.where(prediction==iris_X_test,1,0).sum()/float(len(iris_X_test))
+		print "Neighbors: %d, Accuracy: %3f" % (k, accuracy)
+	print np.average(accuracy)
+	print "Average accuracy: " + str(np.average(accuracy)) + "% over " + str(n) + " runs"
 
-# Create the classifier
-
-results=[ ]
-for n in enumerate(iris_X_train):
-	knn = KNeighborsClassifier(n_neighbors=n)
-	knn.fit(iris_X_train,iris_Y_train)
-	prediction=knn.predict(iris_X_test)
-	#return the predicted results
-	accuracy=np.where(prediction==iris_X_test,1,0).sum()/float(len(iris_X_test))
-	print "Neighbors: %d, Accuracy: %3f" % (n, accuracy)
-	results.append([n,accuracy])
-results=pd.DataFrame(results,columns=["n","accuracy"])
-
-print results
+print nFoldSplit(5)
 
